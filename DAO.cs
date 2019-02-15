7c76e3c33b75e40bf086b990e23ea11aa3b5e7b3
yyassine <39714511+yyassine@users.noreply.github.com>
2019-02-15 17:33:27 +0100

    Initial commit

---
+++ DAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestnote
+{
+    abstract class DAO : MySql
+    {
+        public static string[] param_co;
+        private string Table;
+        //Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+        public DAO(string BD, string UID, string Passw,string table): base(BD,UID,Passw) 
+        {
+            Table = table;
+            // server = localhost;
+            param_co[0] = BD;
+            param_co[1] = UID;
+            param_co[2] = Passw;
+        }
+         public List<Dictionary<string, string>> Select(string Conditions)
+        {
+
+        }
+        public int Update(string action, Dictionary<string,object> Data, string Conditions)
+        {
+
+        }
+    }
+}
---
+++ Mysql.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gestnote
+{
+    abstract class MySql
+    {
+        private string BD;
+        private MySqlConnection con;
+        private string Pass;
+        private MySqlDataReader rs;
+        private MySqlCommand st;
+        private string User;
+
+        public List<Dictionary<string,string>> Get(string sql)
+        {
+
+        }
+
+        protected MySql(string BD, string UID, string Passw)
+        {
+
+        }
+    }
+}
